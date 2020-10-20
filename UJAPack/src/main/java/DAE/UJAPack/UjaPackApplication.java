40c1da9e98b1e6bed377b196c8bd7f2ed7aa1f06
jfrr0006 <jfrr0006@red.ujaen.es>
2020-10-20 12:43:26 +0200

    Proyecto base
    Creación del proyecto base con la ayuda de Spring Initializr.

---
+++ UJAPack/src/main/java/DAE/UJAPack/UjaPackApplication.java
@@ -0,0 +1,13 @@
+package DAE.UJAPack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjaPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UjaPackApplication.class, args);
+	}
+
+}
---
+++ UJAPack/src/test/java/DAE/UJAPack/UjaPackApplicationTests.java
@@ -0,0 +1,13 @@
+package DAE.UJAPack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjaPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
